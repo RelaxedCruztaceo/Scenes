89c4c379522019a8019ccb2b451252d2799bcd1f
Manuel Alejandro Cruz Valladares <133974060+RelaxedCruztaceo@users.noreply.github.com>
2025-11-07 10:51:18 +0100

    Initial Commit

---
+++ Scenes/ScenesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScenesApp.swift
+//  Scenes
+//
+//  Created by Manuel Alejandro Cruz Valladares on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ScenesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
